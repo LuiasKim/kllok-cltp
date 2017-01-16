3979c40a314df41581b9a3bbb7076bb53097704d
LuiasKim <sksd321@naver.com>
2017-01-16 16:07:32 +0900

    asd
    asd

---
+++ Simulator/desktop/src/com/korea/univ/luias/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.korea.univ.luias.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.korea.univ.luias.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
